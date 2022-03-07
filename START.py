0e9f97b4b3a557c1b98ce5d0b5c831129c9b5423
CreateSource <abnormalhare822@gmail.com>
2022-03-07 15:08:22 -0500

    Update START.py

--- START.py
+++ START.py
@@ -12,29 +12,23 @@ console = Console(width=width)
 def terrainGen():
     global terrainList
     terrainList[0] = random.randint(1,2)
-    for i in range(fov):
+    for i in range(fov - 1):
         terrainList += " "
         terrainList[i+1] = terrainList[i] + random.randint(-1,1)
-    check = 1
-    for i in range(fov):
-        if check > terrainList[i]:
-            check = terrainList[i]
-    for j in range(abs(check)):
-        if check < 0:
-            for i in range(fov):
-                terrainList[i] += abs(check)
-    check = 0
-    for i in range(fov):
-        if check < terrainList[i]:
-            check = terrainList[i]
-    k = check
-    for j in range(check):
+    check = max(terrainList)
+    count = min(terrainList)
+    if count < 1:
+        check2 = check + abs(count) + 1
+    else:
+        check2 = check
+    print("")
+    for j in range(check2):
         for i in range(fov):
-            if terrainList[i] >= k:
+            if terrainList[i] >= check:
                 print(0, end='')
             else:
                 print(" ", end='')
-        print("\n")
-        k -= 1
+        print("")
+        check -= 1
 
 terrainGen()
